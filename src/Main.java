fdae8f51ca9affa61acc6f5a770bae18f1fe612e
100128469 <parth.mittal0115@outlook.com>
2022-09-01 09:53:03 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" ____ ");
+        System.out.println("|-  -|");
+        System.out.println("| <> |");
+        System.out.println("|----|________|");
+        System.out.println("(_||_)________|");
+        System.out.println("|_||_|");
+        System.out.println("This platypus is pretty.");
+    }
+}
